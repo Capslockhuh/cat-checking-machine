2a575b974b90a27812dd5315281c78e900c6bb6b
capslockhuh <99589445+Capslockhuh@users.noreply.github.com>
2022-02-14 15:05:11 +0100

    Add files via upload

---
+++ cats.py
@@ -0,0 +1,7 @@
+
+cats = ['Oreo', 'Milki']
+catName = input()
+if catName in cats:
+    print('I do have a cat named ' + catName)
+elif catName not in cats:
+    print('I do not have a cat named ' + catName)
